6b426fb41e4298f66615b6499681f7451c06f5ac
Abdelrahman Fathy <abdofathy883@gmail.com>
2025-09-15 15:59:56 +0300

    fixed logging system

--- Infrastructure/Services/Auth/JWTService.cs
+++ Infrastructure/Services/Auth/JWTService.cs
@@ -3,14 +3,10 @@ using Core.Models;
 using Core.Settings;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
-using System;
-using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
 using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Infrastructure.Services.Auth
 {
