ba965e98ea6a8d0779d68a8d6636cb259e12cad7
Abdelrahman Fathy <abdofathy883@gmail.com>
2026-02-08 18:23:17 +0200

    added authorize to leads controller

--- ClientAPI/Controllers/LeadsController.cs
+++ ClientAPI/Controllers/LeadsController.cs
@@ -1,5 +1,6 @@
 ﻿using Core.DTOs.Leads;
 using Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 using System.Text.Json;
@@ -8,6 +9,7 @@ namespace ClientAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class LeadsController : ControllerBase
     {
         private readonly ILeadService leadService;
